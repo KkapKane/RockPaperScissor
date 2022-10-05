2d7bda0319bbfcf45383aba7f755e447b319fe68
Kane <LisaxKane@gmail.com>
2022-10-04 23:31:58 -0500

    Fully working logic

--- main.js
+++ main.js
@@ -10,33 +10,33 @@ function playRound(playerSelection, computerSelection) {
     if (computerSelection === "Rock" && playerSelection === "Scissors") {
     console.log ("You Lose! Rock beats Scissors");
      playerwin = false;
-     return false;
+     return 'bot';
     }
     else if (computerSelection === "Scissors" && playerSelection === "Paper") {
     console.log ("You Lose! Scissors beats Paper");
      playerwin = false;
-     return false;
+     return 'bot';
     }
     else if (computerSelection === "Paper" && playerSelection === "Rock") {
     console.log ("You Lose! Paper beats Rock");
      playerwin = false;
-     return false;
+     return 'bot';
     }
 
     else if (computerSelection === "Paper" && playerSelection === "Scissors") {
     console.log ("You Win! Scissors beats Paper");
      playerwin = true;
-     return true;
+     return 'player';
     }
     else if (computerSelection === "Rock" && playerSelection === "Paper") {
     console.log ("You Win! Paper beats Rock");
      playerwin = true;
-     return true;
+     return 'player';
     }
     else if (computerSelection === "Scissors" && playerSelection === "Rock") {
     console.log ("You Win! Rock beats Scissors");
      playerwin = true;
-     return true;
+     return 'player';
     }
 
         else 
@@ -58,18 +58,16 @@ return(playerwin)
 let playerlose = 0;
 let playerscore = 0;
 const playerSelection = "Scissors";
-const computerSelection = getComputerChoice();
+let computerSelection = getComputerChoice();
 console.log(playerSelection);
 let findWinner = (playRound(playerSelection, computerSelection));
 console.log(findWinner);
 
 function game(){
-for (let i = 0; i < 5; i++) {
-    findWinner
-    if (findWinner === true){
-        playerscore++;
-    }
-    console.log(playerscore)
+for (let i = 1; i < 5; i++) {
+    computerSelection = getComputerChoice();
+    console.log("round " + i + " " + (computerSelection)) + playRound(playerSelection, computerSelection);
+    
    
     
  }
